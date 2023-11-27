424e62ed5336c8dfb0e5f56c152d739f3cfcb67a
mataramis <buattryout44@gmail.com>
2023-11-27 17:17:52 +0700

    update

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(cors())
+
+app.post('/', (req, res) => {
   res.json('Hello World!')
 })
 
-app.get('/about', (req, res) => {
+app.get('/login', (req, res) => {
+    
+    const username = 'zale';
+    const password = 'zale';
+
+    if(req.body.username === username){
+        if(req.body.username === username){
+            res.json({
+                status : 'succes',   
+            })
+        } else {
+            res.json({
+                status : 'wrong password',   
+            })
+        }
+    } else  {
+        res.json({
+            status : 'error, username not found',   
+        })
+    }
+    
+   
     res.json({
-        page : 'about',
         status : 'succes',   
     })
   })  
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
